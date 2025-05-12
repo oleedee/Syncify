e0f688b6d0aa9b5204db4c929e91b876534c08ee
THEBRYANSMITH <bryanstechinfo@gmail.com>
2025-05-11 23:30:32 -0400

    ADDED BACKEND (2ND ATTEMPT)

---
+++ Backend/Syncify/src/test/java/com/PlaylistTransfer/Syncify/SyncifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PlaylistTransfer.Syncify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
