d2f477ef89c951446172f3bfdbe2314568ecaa5e
oleedee <oneilleedee@gmail.com>
2025-05-18 19:46:47 -0400

    Buttons for settings page

---
+++ src/components/Settings.jsx
@@ -0,0 +1,17 @@
+function Settings() {
+        return ( 
+        <div className = "page-wrapper">
+                <div className = "Leftbar">
+                <div className = "Title">Settings</div>
+                    <div className = "Buttons">
+                        <button className = "Button">Link Accounts</button>
+                        <button className = "Button">Appearance</button>
+                    </div>
+                </div> 
+                <div className = "Mainbox"></div>
+
+                </div>
+        )
+}
+
+export default Settings;
