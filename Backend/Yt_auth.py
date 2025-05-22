bc8ce9963a0bbc07916ecf61b0fe7b350089ba17
THEBRYANSMITH <bryanstechinfo@gmail.com>
2025-05-22 13:28:58 -0400

    Added function to check User credentials and refresh expired youtube user tokens

--- Backend/Yt_auth.py
+++ Backend/Yt_auth.py
@@ -5,5 +5,24 @@ from flask import session, redirect, flash
 
 def youtube_oauth():
     flow = InstalledAppFlow.from_client_secrets_file('clients_secret.json',
-                                                     redirect_uri= )
+                                                     redirect_uri='http://127.0.0.1:5000/redirectyoutube',
+                                                      scopes=['https://www.googleapis.com/auth/youtubepartner',
+                                                        'https://www.googleapis.com/auth/youtube',
+                                                        'https://www.googleapis.com/auth/youtube.force-ssl'] )
+    return  flow
+
+
+def check_yt():
+    credentials = session.get('yt_token_info', None)
+
+    if not credentials or not credentials.valid:
+        if credentials and credentials.expired and credentials.refresh_token:
+            credentials.refresh(Request())
+        else:
+            flash('Youtube account authorization needed!')
+            return redirect('/')
+        
+        session['yt_token_info'] = credentials
+
+
 
