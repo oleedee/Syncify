6a7d4a1f0466eb7c0faffe3c49d8f88e0ca78ad6
THEBRYANSMITH <bryanstechinfo@gmail.com>
2025-05-28 13:43:11 -0400

    Adding functionality to main.py

--- Backend/main.py
+++ Backend/main.py
@@ -2,3 +2,7 @@ from flask import Flask, request, session, redirect, render_template, flash
 from flask_session import Session
 from spotify_auth import create_spotify_oauth, get_spotify_user, check_spot
 from Yt_auth import youtube_oauth, check_yt, get_yt_user
+
+app = Flask(__name__)
+app.jinja_env.filters["track_time"] = time_track
+app.jinja_env.filters["play_time"] = time_play
