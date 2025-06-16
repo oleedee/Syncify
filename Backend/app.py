664c2441a8f4b0c574027d3f77864abe22ad8832
THEBRYANSMITH <bryanstechinfo@gmail.com>
2025-06-15 23:31:39 -0400

    Added code to test endpoints and added firebase to requirements.txt

--- Backend/app.py
+++ Backend/app.py
@@ -9,6 +9,13 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+@app.route('/')
+def index():
+    return "Waagwan!"
+
+@app.route('/hello')
+def hello():
+    return "Bryan, You're the greatest!"
 
 
 if __name__ == '__main__':
